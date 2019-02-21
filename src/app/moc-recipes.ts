0493e9e5c616bd6483482f10a67082ec33bebe17
Mayuri Dua <mayuri.dua@auto-grid.com>
2019-02-21 18:42:14 -0500

    added recipe component

---
+++ src/app/moc-recipes.ts
@@ -0,0 +1,8 @@
+import { Recipe } from './recipe';
+
+export const RECIPES: Recipe[] = [
+  { name: 'Tacos', photo: 'https://www.google.com', ingredients: 'Meat, Tortilla, Cheese, Sour Cream, Pico, Guac', instructions: 'Put it all together.', isSelected: true, isEditing: true },
+  { name: 'Tacos', photo: 'https://www.google.com', ingredients: 'Meat, Tortilla, Cheese, Sour Cream, Pico, Guac', instructions: 'Put it all together.', isSelected: false, isEditing: false },
+  { name: 'Tacos', photo: 'https://www.google.com', ingredients: 'Meat, Tortilla, Cheese, Sour Cream, Pico, Guac', instructions: 'Put it all together.', isSelected: false, isEditing: false },
+  { name: 'Tacos', photo: 'https://www.google.com', ingredients: 'Meat, Tortilla, Cheese, Sour Cream, Pico, Guac', instructions: 'Put it all together.', isSelected: false, isEditing: false }
+];
